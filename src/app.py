8e2badcea7f0d5069ff3ba8e5f8148fc3721afdf
tiyn <tiyn@mail-mk.eu>
2022-07-29 16:59:44 +0200

    src: changed " and \'

--- src/app.py
+++ src/app.py
@@ -13,41 +13,41 @@ WEBSITE = config.WEBSITE
 
 @app.errorhandler(404)
 def page_not_found(e):
-    return render_template('error.html', title=TITLE, errorcode='404', style=STYLE), 404
+    return render_template("error.html", title=TITLE, errorcode="404", style=STYLE), 404
 
 
-@app.route('/')
-@app.route('/index.html')
+@app.route("/")
+@app.route("/index.html")
 def index():
     content = con_gen.gen_index_string()
-    return render_template('index.html', title=TITLE, content_string=content, style=STYLE)
+    return render_template("index.html", title=TITLE, content_string=content, style=STYLE)
 
 
-@app.route('/archive')
-@app.route('/archive.html')
+@app.route("/archive")
+@app.route("/archive.html")
 def blog_archive():
     content = con_gen.gen_arch_string()
-    return render_template('archive.html', title=TITLE, content_string=content, style=STYLE)
+    return render_template("archive.html", title=TITLE, content_string=content, style=STYLE)
 
 
-@app.route('/entry/<path>')
+@app.route("/entry/<path>")
 def entry(path):
     content = con_gen.gen_stand_string(path)
-    if content != '':
-        return render_template('standalone.html', title=TITLE, content_string=content, style=STYLE)
+    if content != "":
+        return render_template("standalone.html", title=TITLE, content_string=content, style=STYLE)
     abort(404)
 
 
-@app.route('/feed.xml')
-@app.route('/rss.xml')
+@app.route("/feed.xml")
+@app.route("/rss.xml")
 def feed():
     content = con_gen.get_rss_string()
-    rss_xml = render_template('rss.xml', content_string=content, title=TITLE,
+    rss_xml = render_template("rss.xml", content_string=content, title=TITLE,
                               description=DESCRIPTION, website=WEBSITE)
     response = make_response(rss_xml)
-    response.headers['Content-Type'] = 'application/rss+xml'
+    response.headers["Content-Type"] = "application/rss+xml"
     return response
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
--- src/config.py
+++ src/config.py
@@ -1,11 +1,11 @@
 # Name/title of your blog
-TITLE = 'Beaker Blog'
+TITLE = "Beaker Blog"
 
 # Description for RSS of your blog
-DESCRIPTION = 'This is your personal Beaker Blog.'
+DESCRIPTION = "This is your personal Beaker Blog."
 
 # URL for your website: e.g. https://domain.tld
-WEBSITE = 'localhost:5000'
+WEBSITE = "localhost:5000"
 
 # Theme for the blog: dark, light
-STYLE = 'dark'
+STYLE = "dark"
