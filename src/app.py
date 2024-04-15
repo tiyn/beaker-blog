a936fd5ee649844b54305d10ab4faca725859bd5
tiyn <tiyn@mail-mk.eu>
2024-04-15 05:16:50 +0200

    fixed minor error with short title

--- src/app.py
+++ src/app.py
@@ -16,32 +16,32 @@ MAIL = config.MAIL
 
 @app.errorhandler(404)
 def page_not_found(e):
-    return render_template("error.html", title=STITLE, errorcode="404", style=STYLE, language=LANGUAGE), 404
+    return render_template("error.html", title=TITLE, stitle=STITLE, errorcode="404", style=STYLE, language=LANGUAGE), 404
 
 
 @app.route("/")
 @app.route("/index.html")
 def index():
     content = con_gen.gen_index_string()
-    return render_template("index.html", title=STITLE, content_string=content, style=STYLE, language=LANGUAGE)
+    return render_template("index.html", title=TITLE, stitle=STITLE, content_string=content, style=STYLE, language=LANGUAGE)
 
 @app.route("/imprint")
 @app.route("/imprint.html")
 def imprint():
-    return render_template("imprint.html", title=STITLE, mail=MAIL, style=STYLE, language=LANGUAGE)
+    return render_template("imprint.html", title=TITLE, stitle=STITLE, mail=MAIL, style=STYLE, language=LANGUAGE)
 
 @app.route("/archive")
 @app.route("/archive.html")
 def archive():
     content = con_gen.gen_arch_string()
-    return render_template("archive.html", title=STITLE, content_string=content, style=STYLE, language=LANGUAGE)
+    return render_template("archive.html", title=TITLE, stitle=STITLE, content_string=content, style=STYLE, language=LANGUAGE)
 
 
 @app.route("/entry/<path>")
 def entry(path):
     content = con_gen.gen_stand_string(path)
     if content != "":
-        return render_template("standalone.html", title=STITLE, content_string=content, style=STYLE, language=LANGUAGE)
+        return render_template("standalone.html", title=TITLE, stitle=STITLE, content_string=content, style=STYLE, language=LANGUAGE)
     abort(404)
 
 
